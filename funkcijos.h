bb1f6edf02adde4da45d906c15707184a366abc2
ievagud <144427953+ievagud@users.noreply.github.com>
2023-12-29 23:55:45 +0200

    Add files via upload

---
+++ funkcijos.h
@@ -0,0 +1,26 @@
+#ifndef FUNKCIJOS_H_INCLUDED
+#define FUNKCIJOS_H_INCLUDED
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
+#include <iostream>
+#include <cctype>
+#include <cctype>
+#include <algorithm>
+#include <iomanip>
+#include <vector>
+#include <regex>
+
+using namespace std;
+
+void Zodziu_sk(string tekstas, string rezultatas);
+bool Skyrybos_zenk(char x);
+void linkai(const string& tekstas, const string& rezultatas);
+
+
+
+
+#endif // FUNKCIJOS_H_INCLUDED
